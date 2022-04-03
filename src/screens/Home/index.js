f30ee5eae3b3edb9b4d091438d90ea699cff8efc
Thanh Doan <thanhdoandev@gmail.com>
2022-04-03 11:42:20 +0700

    feat: [LAP-icons] add icon font

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,10 +1,12 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 const Home = () => {
   return (
     <View style={styles.container}>
       <Text>index</Text>
+      <Icon name="search" />
     </View>
   );
 };
